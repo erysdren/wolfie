a987c322fa2ee43c61721e55f1bfc2a0b0a8f931
Jaycie Ewald <jaycie.ewald@outlook.com>
2023-12-24 17:21:06 -0600

    Initial commit

---
+++ source/main_sdl2.c
@@ -0,0 +1,160 @@
+/*
+MIT License
+
+Copyright (c) 2023 erysdren (it/she/they)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+/*
+ *
+ * MAIN_SDL2.C
+ *
+ */
+
+#include <stdio.h>
+#include <stdint.h>
+
+/* eui */
+#include "eui_sdl2.h"
+
+/* wolfie */
+#include "wolfie.h"
+
+/* wolfenstein 3d palette */
+#include "palette_wolf3d.h"
+
+/* SDL state */
+static SDL_Window *window;
+static SDL_Surface *surface8;
+static SDL_Surface *surface32;
+static SDL_Renderer *renderer;
+static SDL_Texture *texture;
+static SDL_Rect blit_rect;
+static SDL_bool running;
+static SDL_Color colors[256];
+
+/* main */
+int main(int argc, char **argv)
+{
+	uint32_t format;
+	unsigned int rmask, gmask, bmask, amask;
+	int bpp;
+	SDL_Event event;
+	int i;
+
+	EUI_UNUSED(argc);
+	EUI_UNUSED(argv);
+
+	/* init */
+	SDL_Init(SDL_INIT_VIDEO);
+
+	/* create window */
+	window = SDL_CreateWindow(WOLFIE_TITLE,
+		SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+		WOLFIE_WIDTH, WOLFIE_HEIGHT,
+		SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI
+	);
+
+	/* set window size */
+	SDL_SetWindowMinimumSize(window, WOLFIE_WIDTH, WOLFIE_HEIGHT);
+
+	/* create renderer */
+	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC);
+	SDL_RenderSetLogicalSize(renderer, WOLFIE_WIDTH, WOLFIE_HEIGHT);
+	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+	SDL_RenderClear(renderer);
+	SDL_RenderPresent(renderer);
+	SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "nearest");
+
+	/* create our render surface */
+	surface8 = SDL_CreateRGBSurface(0, WOLFIE_WIDTH, WOLFIE_HEIGHT, 8, 0, 0, 0, 0);
+	SDL_FillRect(surface8, NULL, 0);
+
+	/* generate palette table */
+	for (i = 0; i < 256; i++)
+	{
+		colors[i].r = palette[i * 3];
+		colors[i].g = palette[i * 3 + 1];
+		colors[i].b = palette[i * 3 + 2];
+	}
+
+	/* install palette */
+	SDL_SetPaletteColors(surface8->format->palette, colors, 0, 256);
+
+	/* create display surface */
+	format = SDL_GetWindowPixelFormat(window);
+	SDL_PixelFormatEnumToMasks(format, &bpp, &rmask, &gmask, &bmask, &amask);
+	surface32 = SDL_CreateRGBSurface(0, WOLFIE_WIDTH, WOLFIE_HEIGHT, bpp, rmask, gmask, bmask, amask);
+	texture = SDL_CreateTexture(renderer, format, SDL_TEXTUREACCESS_STREAMING, WOLFIE_WIDTH, WOLFIE_HEIGHT);
+
+	/* make sure relative mouse mode is disabled */
+	SDL_SetRelativeMouseMode(SDL_FALSE);
+
+	/* setup blit rect */
+	blit_rect.x = 0;
+	blit_rect.y = 0;
+	blit_rect.w = WOLFIE_WIDTH;
+	blit_rect.h = WOLFIE_HEIGHT;
+
+	/* main loop */
+	running = SDL_TRUE;
+	while (running)
+	{
+		/* parse sdl events */
+		while (SDL_PollEvent(&event))
+		{
+			eui_push_event_sdl2(&event);
+
+			switch (event.type)
+			{
+				case SDL_QUIT:
+					running = SDL_FALSE;
+					break;
+			}
+		}
+
+		/* begin */
+		if (eui_begin_sdl2(surface8))
+		{
+			/* clear */
+			eui_clear(0);
+
+			/* eui */
+			eui_end();
+		}
+
+		/* blit to screen */
+		SDL_BlitSurface(surface8, &blit_rect, surface32, &blit_rect);
+		SDL_UpdateTexture(texture, NULL, surface32->pixels, surface32->pitch);
+		SDL_RenderClear(renderer);
+		SDL_RenderCopy(renderer, texture, NULL, NULL);
+		SDL_RenderPresent(renderer);
+	}
+
+	/* quit */
+	SDL_FreeSurface(surface8);
+	SDL_FreeSurface(surface32);
+	SDL_DestroyTexture(texture);
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+
+	return 0;
+}
